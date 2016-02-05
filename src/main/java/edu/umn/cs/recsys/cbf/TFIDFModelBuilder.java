a2bf540d69477fd18a081bbe8afcab11f9834c77
ad <jee45@txstate.edu>
2016-02-05 12:02:46 -0600

    added example code form class

--- src/main/java/edu/umn/cs/recsys/cbf/TFIDFModelBuilder.java
+++ src/main/java/edu/umn/cs/recsys/cbf/TFIDFModelBuilder.java
@@ -18,6 +18,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+
 /**
  * Builder for computing {@linkplain TFIDFModel TF-IDF models} from item tag data.  Each item is
  * represented by a normalized TF-IDF vector.
@@ -72,8 +78,85 @@ public class TFIDFModelBuilder implements Provider<TFIDFModel> {
             // Create a work vector to accumulate this item's tag vector.
             Long2DoubleMap work = new Long2DoubleOpenHashMap();
 
-            // TODO Populate the work vector with the number of times each tag is applied to this item.
+            // TODO this came from the example file in class.
+            //it is prolly not currently correct yet
             //fart big time
+
+            File file = new File("messages");
+            // Store the vector for each file (by its file name)
+            Map<String, Map<String,Integer>> documents = new HashMap<>();
+            // Loop over all the files
+            for (File msgFile: file.listFiles()) {
+                logger.debug("will read file {}", msgFile.getName());
+                // Create new empty map to store word counts
+                Map<String, Integer> terms = new HashMap<>();
+                // Open the file
+                try (Scanner scan = new Scanner(msgFile)) {
+                    // Separate words with non-word characters
+                    scan.useDelimiter("\\W+");
+                    // Get each word
+                    while (scan.hasNext()) {
+                        String word = scan.next().toLowerCase();
+                        // Count it!
+                        if (terms.containsKey(word)) {
+                            terms.put(word, 1 + terms.get(word));
+                        } else {
+                            terms.put(word, 1);
+                        }
+                    }
+                } catch (FileNotFoundException e) {
+                    logger.error("Could not find file {}", msgFile);
+                    System.exit(2);
+                }
+
+                documents.put(msgFile.getName(), terms);
+            }
+
+            logger.info("read " + documents.size() + " documents");
+
+            // Create list to accumulate results
+            List<SearchResult> results = new ArrayList<>();
+            // Loop over all file names
+            for (String fn: documents.keySet()) {
+                // Get file's term vector
+                Map<String, Integer> fileTerms = documents.get(fn);
+                // initialize score to 0
+                double score = 0;
+                for (String qw: args) {
+                    String qwlc = qw.toLowerCase();
+                    // how many times?
+                    Integer count = fileTerms.get(qwlc);
+                    // count is null if it isn't there, a count if it is
+                    if (count != null) {
+                        score += count;
+                    }
+                }
+
+                if (score > 0) {
+                    // add the result to the list!
+                    results.add(new SearchResult(fn, score));
+                }
+            }
+
+            // sort results with our comparisons
+            Collections.sort(results, new SearchResult.ScoreComparator());
+
+            // and now print the first so many
+            int n = 0;
+            for (SearchResult res: results) {
+                System.out.format("%s: %f\n", res.getFileName(), res.getScore());
+                n++;
+                if (n >= 10) {
+                    break;
+                }
+            }
+
+
+
+
+
+
+
             // TODO Increment the document frequency vector once for each unique tag on the item.
 
             // Save the vector in our map, we'll add IDF and normalize later.
@@ -94,6 +177,8 @@ public class TFIDFModelBuilder implements Provider<TFIDFModel> {
         Map<Long,Long2DoubleMap> modelData = new HashMap<>();
         for (Map.Entry<Long,Long2DoubleMap> entry: itemVectors.entrySet()) {
             Long2DoubleMap tv = new Long2DoubleOpenHashMap(entry.getValue());
+
+
             // TODO Convert this map (vector) to a TF-IDF vector
             // TODO Normalize the TF-IDF vector to be a unit vector
             // HINT Take a look at the Vectors class in org.lenskit.util.math.
@@ -111,6 +196,9 @@ public class TFIDFModelBuilder implements Provider<TFIDFModel> {
      *
      * @return A mapping from tags to IDs.
      */
+
+
+
     private Map<String,Long> buildTagIdMap() {
         // Get the universe of all tags
         Set<String> tags = itemTagDAO.getTagVocabulary();
